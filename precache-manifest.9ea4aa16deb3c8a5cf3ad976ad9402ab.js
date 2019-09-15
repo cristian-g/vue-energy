5b9c145748c97d22f39713f2bec6de539380c65b
cristian-g <cristian@cristiangonzalez.com>
2019-09-16 01:36:56 +0200

    Build

---
+++ precache-manifest.9ea4aa16deb3c8a5cf3ad976ad9402ab.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "464e48b4a9ba63677020",
+    "url": "/vue-energy/css/app.04d5b109.css"
+  },
+  {
+    "revision": "464e48b4a9ba63677020",
+    "url": "/vue-energy/js/app.c7abe590.js"
+  },
+  {
+    "revision": "58369b90c8ee61455367",
+    "url": "/vue-energy/js/chunk-vendors.abb63db7.js"
+  },
+  {
+    "revision": "60c67e7dc1a40278c315",
+    "url": "/vue-energy/js/demo.19b19a48.js"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/vue-energy/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/vue-energy/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/vue-energy/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/vue-energy/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/vue-energy/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "bc605129796ce20de2653cde1be5a072",
+    "url": "/vue-energy/index.html"
+  },
+  {
+    "revision": "a794a15f6e45b941d8a521e7965221a8",
+    "url": "/vue-energy/img/brand/favicon.png"
+  },
+  {
+    "revision": "78570fcb515a5545740db21422a89212",
+    "url": "/vue-energy/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "09c31792a721f9a6d5e59d90e83e483d",
+    "url": "/vue-energy/img/theme/profile-cover.jpg"
+  },
+  {
+    "revision": "a9f4d915b0523b576f808fd287c276ac",
+    "url": "/vue-energy/img/theme/profile.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-energy/robots.txt"
+  },
+  {
+    "revision": "fc00841b34fbc9223d90c7c525d580c6",
+    "url": "/vue-energy/prices.json"
+  }
+];
